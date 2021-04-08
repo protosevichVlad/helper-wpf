44e7d3b1eef27f0cb5cc2cd9a9c7602725402144
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-04-08 23:31:10 +0300

    Create NewsService.

---
+++ HelperWPF/Services/NewsService.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelperWPF.Interfaces;
+
+namespace HelperWPF.Services
+{
+    public class NewsService
+    {
+        private readonly IRepository<News> _data;
+
+        public NewsService(IRepository<News> data)
+        {
+            _data = data;
+        }
+        
+        public async Task<List<Weather>> GetAllNews()
+        {
+            return (await _data.GetAll()).ToList();
+        }
+    }
+}
